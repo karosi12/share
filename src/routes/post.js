ad27400436e2d91aa4b9a17b2cc9cbea7294adb4
karosi12 <adekayor@gmail.com>
2021-04-30 23:13:19 +0100

    refactor route format

--- src/routes/post.js
+++ src/routes/post.js
@@ -3,14 +3,14 @@ import authenticate from "../middlewares/authenticate";
 import postCtrl from "../controller/post";
 const postRouter = express.Router();
 
-postRouter.post("/api/post", authenticate, postCtrl.create);
-postRouter.get("/api/posts", postCtrl.list);
-postRouter.get("/api/post/:id", postCtrl.view);
-postRouter.patch("/api/post/like", authenticate, postCtrl.like);
-postRouter.patch("/api/post/undolike", authenticate, postCtrl.undoLike);
-postRouter.patch("/api/post/comment", authenticate, postCtrl.comment);
-postRouter.patch("/api/post/subscribe", authenticate, postCtrl.subscribe);
-postRouter.put("/api/post/:postId", authenticate, postCtrl.updatePost);
-postRouter.delete("/api/post/:postId", authenticate, postCtrl.deletePost);
+postRouter.post("/post", authenticate, postCtrl.create);
+postRouter.get("/posts", postCtrl.list);
+postRouter.get("/post/:id", postCtrl.view);
+postRouter.patch("/post/like", authenticate, postCtrl.like);
+postRouter.patch("/post/undolike", authenticate, postCtrl.undoLike);
+postRouter.patch("/post/comment", authenticate, postCtrl.comment);
+postRouter.patch("/post/subscribe", authenticate, postCtrl.subscribe);
+postRouter.put("/post/:postId", authenticate, postCtrl.updatePost);
+postRouter.delete("/post/:postId", authenticate, postCtrl.deletePost);
 
 export default postRouter;
--- src/routes/user.js
+++ src/routes/user.js
@@ -3,10 +3,10 @@ import userCtrl from "../controller/user";
 import authenticate from "../middlewares/authenticate";
 const userRouter = express.Router();
 
-userRouter.post("/api/login", userCtrl.login);
-userRouter.post("/api/signup", userCtrl.register);
-userRouter.put("/api/change_password", userCtrl.resetPassword);
-userRouter.get("/api/users", authenticate , userCtrl.list);
-userRouter.get("/api/user/:id" , userCtrl.find);
+userRouter.post("/login", userCtrl.login);
+userRouter.post("/signup", userCtrl.register);
+userRouter.put("/change_password", userCtrl.resetPassword);
+userRouter.get("/users", authenticate , userCtrl.list);
+userRouter.get("/user/:id" , userCtrl.find);
 
 export default userRouter;
